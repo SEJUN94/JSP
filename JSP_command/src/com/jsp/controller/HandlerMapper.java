bf5182ef52657fdb9dec770b6cd589ba8f44dfa0
PC-14 <PC-14@DESKTOP-KUCU871>
2022-02-16 10:40:38 +0900

    2/16

--- JSP_command/src/com/jsp/controller/HandlerMapper.java
+++ JSP_command/src/com/jsp/controller/HandlerMapper.java
@@ -1,17 +1,40 @@
 package com.jsp.controller;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
 
 import com.jsp.action.Action;
 
 public class HandlerMapper {
-	private String PATH = "com/jsp/properties/url"; // 리소스 번들은 .properties를 빼고 작성해야 이터레이터방식으로 읽어들임
+	private static final String path = "com/jsp/properties/url"; // 리소스 번들은 .properties를 빼고 작성해야  읽어들임
 	
 	private Map<String, Action> commandMap = new HashMap<String, Action>();
 	
+	public HandlerMapper() throws Exception{
+		this(path);
+	}
+	
 	public HandlerMapper(String path) throws Exception{
 		
+		ResourceBundle rbHome = ResourceBundle.getBundle(path);
+		
+		Set<String> actionSetHome = rbHome.keySet(); // uri set
+		
+		Iterator<String> it = actionSetHome.iterator();
+		while(it.hasNext()) {
+			String command = it.next(); // key -> url
+			String actionClassName = rbHome.getString(command);
+			
+			Class<?> actionClass = Class.forName(actionClassName);
+			Action commandAction = (Action) actionClass.newInstance();
+			
+			commandMap.put(command, commandAction);
+			System.out.println("[HandlerMapper]" + command + ":" + commandAction + " 가 준비되었습니다.");
+		}
+	
 	}
 	
 }
