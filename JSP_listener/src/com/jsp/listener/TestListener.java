a4b063674d623730c3b519468988f037611c7ce7
PC-14 <PC-14@DESKTOP-KUCU871>
2022-02-17 13:46:52 +0900

    2/17

---
+++ JSP_listener/src/com/jsp/listener/TestListener.java
@@ -0,0 +1,27 @@
+package com.jsp.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+
+public class TestListener implements ServletContextListener {
+
+
+	
+    public void contextDestroyed(ServletContextEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent event)  { 
+         System.out.println("TestListener loading !!!!!");
+         
+         String message = event.getServletContext().getInitParameter("message");
+         System.out.println(message);
+         
+    }
+	
+}
